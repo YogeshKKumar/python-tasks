d75b441344d37fe6deee75b4473f23a628566bd7
YogeshKKumar <123815988+YogeshKKumar@users.noreply.github.com>
2026-03-12 12:28:22 +0530

    Add files via upload

---
+++ python_tasks.py
@@ -0,0 +1,76 @@
+#TASK1
+print("Hello", "World", "Welcome", "Python")
+print("Laptop", "Mouse", "Keyboard", sep=" | ")
+
+#TASK2
+name = "Ravi"
+age = 22
+city = "Chennai"
+print(name, age, city, sep="-")
+
+#TASK3
+name, age, student = "Meena", 20, True
+print(name, age, student)
+
+#TASK4
+word = "Python"
+print(word[0])
+print(word[2])
+print(word[-1])
+
+#TASK5
+print(25 + 10)
+print(50 - 20)
+print(8 * 5)
+print(100 / 10)
+print(10 % 3)
+print(2 ** 4)
+print(20 // 3)
+
+#TASK6
+print(3 + 2 * 5 ** 2)
+
+#TASK7
+num = 50
+num += 25
+print(num)
+
+num = 100
+num /= 10
+print(num)
+
+#TASK8
+print(10 > 5)
+print(20 < 15)
+print(5 == 5)
+print(10 != 8)
+print(7 >= 7)
+print(6 <= 2)
+
+#TASK9
+a = "apple"
+b = "Apple"
+print(a == b)
+
+#TASK10
+print(10 > 5 and 5 == 5)
+print(5 > 10 or 10 == 10)
+print(not(5 > 2))
+
+#TASK11
+numbers = [10,20,30,40,50]
+print(20 in numbers)
+print(60 in numbers)
+print(30 not in numbers)
+
+#TASK12
+a = 10
+b = 20
+a, b = b, a
+print("a =", a)
+print("b =", b)
+
+#TASK13
+a = 6
+b = 3
+print(a ^ b)
